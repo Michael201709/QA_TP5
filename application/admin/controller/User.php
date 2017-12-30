da0123aff877d8e075100192bdd0dc6905932dca
Michael_26 <mashuai21@live.com>
2017-12-30 20:23:28 +0800

    修正 app和public
    Signed-off-by: Michael_26 <mashuai21@live.com>

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -10,12 +10,6 @@ class User extends Base
 {
     public function index()
     {
-
-        // DB的方法查询管理员，除了userpass这个字段
-        // $list = Db::table('lamp_admin')->select();
-        // dump($list);die;
-        // $arr = Db::table('lamp_user')->select();
-
         // 助手函数查询 排除userpass字段
         $admins = Db('admin')->field('userpass', true)->select();
         $arr = Db('user')->field('userpass', true)->select();
@@ -27,31 +21,22 @@ class User extends Base
             $rids = Db('user_role')->field('rid')->where(array(
                 'uid' => $v['id']))
                 ->select();
-            // dump($rids);
-            // die;
 
             // 如果未分配权限，则需要事先声明一个空数组
             $rnames = array();
 
             // 遍历
             foreach ($rids as $value) {
-                // 查询"角色表",当rids存在于role表中，查询role_name即角色名称，可能为一个数组
-                // $rnames = Db('role')->field('name')->where(['id' => $v['rid'], 'status' => 1])->select();
-
                 $rnames[] = Db('role')->where(array(
                     'id' => array('eq', $value['rid']),
                     'status' => array('eq', 1)))
                     ->value('name');
-
             }
-            // dump($rnames);
-            // die;
             // 将查询到的角色名称[]塞入用户表中,组成一个新的表。
             $v['rname'] = $rnames;
             $list[] = $v;
         }
         // 将组建的新表返回html文件中，遍历
-        // dump($list);
         return view('admin@main/user', [
             'title' => "用户管理",
             'list' => $list,
@@ -61,7 +46,6 @@ class User extends Base
 
     public function save(Request $request)
     {
-        // dump($_POST);
         $p = $request->post();
 
         $data = [
@@ -70,7 +54,6 @@ class User extends Base
             'name' => $p['name'],
             'sex' => $p['sex']
         ];
-
         // 执行添加保存
         $result = Db::name('admin')->data($data)->insert();
 
@@ -91,54 +74,20 @@ class User extends Base
     public function edit($id)
     {
         // 查询该用户所拥有的所有信息；用户表、角色表联查
-
-        // $data = Db::view('admin', 'username,name,sex')
+        // $data = Db::view('admin', 'id,username,name,sex')
         //     ->view('user_role', 'rid,uid', 'uid=user.id')
-        //     ->view('role', 'name,status', 'role.uid=rid');
+        //     ->view('role', ['name'=>'rname'], 'role.uid=rid');
         //
         // dump($data);
         // die();
 
-        // $admins = Db('admin')->field('userpass', true)->where(array('id' => $id))->select();
-        // foreach ($admins as $v) {
-        //     $rids = Db('user_role')->field('rid')->where(array(
-        //         'uid' => $v['id']))
-        //         ->select();
-        //     $rnames = array();
-        //     foreach ($rids as $value) {
-        //         $rnames[] = Db('role')->where(array(
-        //             'id' => array('eq', $value['rid'])))
-        //             ->value('name');
-        //     }
-        //     $v['rname'] = $rnames;
-        //     $list[] = $v;
-        // }
-        // dump($list);
-        // die();
-
         $admins = Db('admin')->field('userpass', true)->where(array('id' => $id))->select();
-
-        // foreach ($admins as $v) {
-        //     for ($i = 0; $i < $arr; $i++) {
-        //
-        //     }
-        // }
-        // dump($list);
         $rnames = Db('role')->field('name')->select();
-
-        //
-        // dump($admins);
-
-        // dump($rnames);
-        // die();
-
         return view('admin@main/edit', [
             'title' => '用户编辑和查看',
             'admins' => $admins,
             'rnames' => $rnames
         ]);
-
-        // return '编辑指定ID：' . $id;
     }
 
     /**
